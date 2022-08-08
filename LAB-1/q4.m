8e2fbc35681a190b9fd1f976330807b026888533
Kanishk Kaushal <75947299+Kanishk-Kaushal@users.noreply.github.com>
2022-08-08 22:59:45 +0530

    Create q4.m

---
+++ LAB-1/q4.m
@@ -0,0 +1,11 @@
+a = 11 / 5 + 6 ;
+b = (11 / 5) + 6;
+c = 11 / (5 + 6);
+d =  round(-11 / 5) + 6 ;
+e = ceil(-11 / 5) + 6;
+f = floor(-11 / 5) + 6;
+g = 3 ^ 2 ^ 3;
+h = 3 ^ (2 ^ 3);
+i = (3 ^ 2) ^ 3;
+
+% Check out "Workspace" for solutions
