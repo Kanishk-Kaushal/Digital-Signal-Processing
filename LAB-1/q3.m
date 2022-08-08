a801bb32bee6de20175bf3cd27bcb6b394a4428f
Kanishk Kaushal <75947299+Kanishk-Kaushal@users.noreply.github.com>
2022-08-08 22:59:08 +0530

    Create q3.m

---
+++ LAB-1/q3.m
@@ -0,0 +1,16 @@
+a = [1 0; 2 1];
+b = [-1 2; 0 1];
+c = [3; 2];
+d = 5;
+
+s1 = a + b;
+s2 = a + c;
+s3 = a + d;
+s4 = a .* b;
+s5 = a * b;
+s6 = a * c;
+s7 = a .* c;
+s8 = a .* d;
+s9 = a * d;
+
+% check out "Workspace" for solutions
